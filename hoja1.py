5273de3854ee13c009d6f9600f31245adc152ec7
sofia.ocampo <sofia.ocampo@accenture.com>
2020-04-12 15:18:29 -0500

    Taller 1

--- hoja1.py
+++ hoja1.py
@@ -1,1 +1,52 @@
 ############## Hoja 1 ###############
+
+# #Area de un triángulo
+# base = float(input("Escriba la base en cm: "))
+# altura = float(input("Escriba la altura en cm: "))
+# area  = float((base*altura)/2)
+# print("El area es: " + str(area))
+
+# #Convertir en pesos
+# dolar = float(input("Escriba el valor en dolares: "))
+# print("Valor en pesos: "+str(dolar/4000))
+
+# #convertir a centigrados
+# n = float(input("Escriba el valor en centigrados: "))
+# print("Valor en Farenheit: "+str((n * 9/5) + 32))
+
+# # lustro en segundos
+# print("Valor de un lustro en segundos: "+ str(5*366*24*60*60))
+
+#Cantidad de segundos que toma la luz viajar a marte
+# print("segundos de la luz a marte "+ str((3*10**8) / 249100))
+
+# Distancia diametro llanta
+# print("Vueltas que da la llanta "+ str(100000/(3.1416*50)))
+
+# #Si la edad es la misma
+# a = int(input("Escriba la edad 1: "))
+# b = int(input("Escriba la edad 2: "))
+# if a==b:
+#     boolean=True 
+# else:
+#     boolean=False
+# print("La edad es la misma: "+ str(boolean))
+
+# meses transcurridos desde una fecha
+# from datetime import datetime
+# from datetime import timedelta
+
+# actual = datetime.now()
+# a = int(input("Escriba el año de su cumpleaños: "))
+# m = int(input("Escriba el mes de su cumpleaños: "))
+# d = int(input("Escriba el dia de su cumpleaños: "))
+# fecha = datetime(a,m,d)
+# meses=(actual - fecha)/30
+# print("meses: "+ meses)
+
+#promedio de notas
+# for i in range(0,5):
+#     nota = float(input("Escriba la nota de la materia: "))
+#     nota +=nota
+# print("El promedio es: "+str(nota/5))
+
